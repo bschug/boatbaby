ac62ddd58a9ffd7f0c35e4411b7bbb3420b8e175
Daniel Helbig <daniel.helbig@gmx.de>
2017-01-20 21:06:27 +0100

    Created Game Scene, added baby and boat, added rotation script for testing purposes

---
+++ Assets/boatRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boatRotation : MonoBehaviour {
+
+	[SerializeField] float _rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ( Input.GetKey( KeyCode.LeftArrow ) || Input.GetKey( KeyCode.A ) ) {
+
+			this.transform.Rotate( new Vector3( 0, 0,  _rotationSpeed * Time.deltaTime) );
+
+			Debug.Log( "Rotate left" );
+		}
+
+		else if ( Input.GetKey( KeyCode.RightArrow ) || Input.GetKey( KeyCode.D ) ) {
+
+			this.transform.Rotate(  new Vector3( 0, 0, - _rotationSpeed * Time.deltaTime) );
+
+			Debug.Log( "Rotate right" );
+		} 
+	}
+}
