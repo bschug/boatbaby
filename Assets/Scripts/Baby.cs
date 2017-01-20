f37cbb89ed1e6c29c194b084a9c587ad68d60d98
Daniel Helbig <daniel.helbig@gmx.de>
2017-01-21 00:12:28 +0100

    Added baby class and eatable items

---
+++ Assets/Scripts/Baby.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baby : MonoBehaviour {
+
+	public void EatThis( EatableItem item ) {
+
+		Debug.Log( "Hamjamham" );
+	}
+}
---
+++ Assets/Scripts/EatableItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatableItem : MonoBehaviour {
+
+	void OnCollisionEnter2D( Collision2D collision ) {
+
+		if ( collision.gameObject.tag == "baby" ) {
+
+			collision.gameObject.GetComponent<Baby>().EatThis( this );
+
+			Destroy( this.gameObject );
+		}
+	}
+}
