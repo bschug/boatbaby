f324cc25e43e212c5993f70c7e86f8ddb647891d
Benjamin Schug <benjamin.schug@gmail.com>
2017-01-21 00:09:39 +0100

    Add GyroGravity and test scene

---
+++ Assets/Scripts/Gyro/GyroGravity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroGravity : SingletonMonoBehaviour<GyroGravity> {
+
+    override protected void Awake() {
+        base.Awake();
+        Input.gyro.enabled = true;
+    }
+
+    void Update() {
+        Physics2D.gravity = AdjustedGravity;
+    }
+
+    Vector2 AdjustedGravity
+    {
+        get
+        {
+            var gravity = Input.gyro.gravity;
+            // Invert gravity if screen is flipped
+            //if (IsScreenFlipped) {
+            //    gravity *= -1;
+            //}
+            // Don't float up if screen is upside down and hasn't auto-rotated yet
+            gravity.y = Mathf.Min(gravity.y, 0);
+            return gravity;
+        }
+    }
+
+    bool IsScreenFlipped { get { return Screen.orientation == ScreenOrientation.LandscapeLeft; } }
+}
