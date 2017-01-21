564e333df25da65c1c2803bc3d43cf77444dec3e
Daniel Helbig <daniel.helbig@gmx.de>
2017-01-21 12:54:57 +0100

    Added touch input and send react animation trigger

---
+++ Assets/Scripts/Input/TouchInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+
+	public GameObject 	_target;
+	Ray 				_ray = new Ray();
+	RaycastHit2D 		_hit = new RaycastHit2D();
+	Camera 				_mainCamera;
+
+
+	// Use this for initialization
+	void Start () {
+
+		_mainCamera = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if ( Input.GetMouseButtonDown( 0 ) == true ) {
+
+			_ray =  _mainCamera.ScreenPointToRay( new Vector2( Input.mousePosition.x, Input.mousePosition.y) );
+
+			_hit = Physics2D.Raycast( _ray.origin, _ray.origin );
+
+			if ( _hit.collider != null && _hit.collider.tag == "baby" ) {
+
+				_target = _hit.collider.gameObject;
+
+				Animator animator = _target.GetComponent<Animator>();
+
+				if ( animator != null ) {
+
+					animator.SetTrigger( "reactAnim" );
+				}
+
+				else {
+
+					Debug.LogWarning( "Target has no animator component" );
+				}
+			}
+		}
+	}
+}
