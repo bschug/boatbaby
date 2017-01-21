63a3a10023b8168083e9f71056c3c9c5bc1f9f91
Daniel Helbig <daniel.helbig@gmx.de>
2017-01-21 16:45:24 +0100

    moved color manager to script folder

---
+++ Assets/Scripts/Baby/ColorManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour {
+
+	public List<Color> _colors;
+	public SpriteRenderer _target;
+
+	public void PickRandomColor() {
+
+		int randomNumber = Random.Range( 0, _colors.Count );
+
+		_target.color = _colors[ randomNumber ];
+	}
+
+
+}
