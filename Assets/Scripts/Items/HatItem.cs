1a7579ddb1686c67f42666770826f8d258972d3b
Benjamin Schug <benjamin.schug@gmail.com>
2017-01-22 02:15:53 +0100

    Wear Hats

--- Assets/Scripts/Items/HatItem.cs
+++ Assets/Scripts/Items/HatItem.cs
@@ -4,7 +4,32 @@ using UnityEngine;
 
 public class HatItem : MonoBehaviour {
 
-	void OnCollisionEnter2D( Collision2D collision ) {
+    Transform FollowPoint = null;
+
+    private void Update () {
+        if (FollowPoint != null) {
+            transform.position = FollowPoint.position;
+            transform.rotation = FollowPoint.rotation;
+        }
+    }
+
+    public void AttachTo (Transform followPoint) {
+        Debug.Log( "Attaching " + name );
+        FollowPoint = followPoint;
+        GetComponent<SpriteRenderer>().sortingLayerName = "character_item_front";
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<PointOfInterest>().enabled = false;
+    }
+
+    public void Detach () {
+        Debug.Log( "Detaching " + name );
+        FollowPoint = null;
+        GetComponent<SpriteRenderer>().sortingLayerName = "waves_items";
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        gameObject.layer = LayerMask.NameToLayer( "ItemInWater" );
+    }
+
+    void OnCollisionEnter2D ( Collision2D collision ) {
 
 		if ( collision.gameObject.tag == "baby" ) {
 
